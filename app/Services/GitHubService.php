db4f0b211889dbf4965262e6232e8d5db200cedf
mkaramica <mahdykaramy@yahoo.com>
2024-03-19 14:14:38 -0400

    Modifying GitHubController...

--- app/Services/GitHubService.php
+++ app/Services/GitHubService.php
@@ -8,12 +8,6 @@ class GitHubService
 {
     protected static $apiUrl = 'https://api.github.com';
 
-    public static function userExists($username)
-    {
-        $response = Http::get(self::$apiUrl . "/users/$username");
-        return $response->successful();
-    }
-    
     public static function fetchUserInfo($username)
     {
         $response = Http::get(self::$apiUrl . "/users/$username");
